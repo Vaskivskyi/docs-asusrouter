bba52dd00c9c6ae93ecf958cdc87d86d19d944e4
Yevhenii Vaskivskyi <yevhenii@vaskivskyi.com>
2023-01-28 19:19:23 +0100

    Fix typo

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -1,5 +1,5 @@
 import { defineClientConfig } from '@vuepress/client'
-import Layout from './layouts/Layout.vue'
+import Layout from './layouts/layout.vue'
 
 export default defineClientConfig({
     layouts: {
