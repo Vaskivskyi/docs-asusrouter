c80c8be8afb744db8d8fe0de0833a22bea01c962
Yevhenii Vaskivskyi <yevhenii@vaskivskyi.com>
2024-08-26 20:18:49 +0200

    Add description of the `firmware` library module (#146)

---
+++ docs/library/modules/README.md
@@ -0,0 +1,8 @@
+# Modules
+
+## List of modules
+
+> [!note]
+> The list of modules is still being updated. Please refer to the [source code](https://github.com/vaskivskyi/asusrouter) if some information is missing.
+
+### [Firmware](/library/modules/firmware.md)
--- sidebar.ts
+++ sidebar.ts
@@ -1,60 +1,66 @@
-import type { SidebarConfig } from "@vuepress/theme-default"
+import type { SidebarConfig } from "@vuepress/theme-default";
 import { getFiles, getFilesPushFront, getFilesRev } from "./navbar";
-import { default as deviceList } from "./docs/devices/map.json" assert { type: "json" }
+import { default as deviceList } from "./docs/devices/map.json" assert { type: "json" };
 
 export const sidebar: SidebarConfig = {
-    "/library/": [
+  "/library/": [
+    {
+      text: "Python Library",
+      link: "/library/",
+      children: [
         {
-            text: "Python Library",
-            link: "/library/",
-            // children: getFiles("library"),
+          text: "Modules",
+          link: "/library/modules/",
+          children: getFiles("library/modules"),
         },
-    ],
-    "/guide/": [
-        {
-            text: "Getting started",
-            link: "/guide/getting-started/",
-        },
-        {
-            text: "Configuration",
-            link: "/guide/configuration/",
-            children: [
-                "/guide/configuration/operation-mode.md",
-                "/guide/configuration/time-constants.md",
-                "/guide/configuration/network-interfaces.md",
-                "/guide/configuration/events.md",
-                "/guide/configuration/security.md",
-            ]
-        },
-        {
-            text: "How To",
-            link: "/guide/how-to/",
-            children: getFiles("guide/how-to"),
-        },
-        {
-            text: "FAQ",
-            link: "/guide/faq/",
-        },
-    ],
-    "/features/": [
-        {
-            text: "Features",
-            link: "/features/",
-            children: getFilesPushFront("features", ["main.md"]),
-        },
-    ],
-    "/devices/": [
-        {
-            text: "Supported devices",
-            link: "/devices/",
-            children: deviceList,
-        },
-    ],
-    "/log/": [
-        {
-            text: "Change log",
-            link: "/log/",
-            children: getFiles("log")
-        },
-    ],
+      ],
+    },
+  ],
+  "/guide/": [
+    {
+      text: "Getting started",
+      link: "/guide/getting-started/",
+    },
+    {
+      text: "Configuration",
+      link: "/guide/configuration/",
+      children: [
+        "/guide/configuration/operation-mode.md",
+        "/guide/configuration/time-constants.md",
+        "/guide/configuration/network-interfaces.md",
+        "/guide/configuration/events.md",
+        "/guide/configuration/security.md",
+      ],
+    },
+    {
+      text: "How To",
+      link: "/guide/how-to/",
+      children: getFiles("guide/how-to"),
+    },
+    {
+      text: "FAQ",
+      link: "/guide/faq/",
+    },
+  ],
+  "/features/": [
+    {
+      text: "Features",
+      link: "/features/",
+      children: getFilesPushFront("features", ["main.md"]),
+    },
+  ],
+  "/devices/": [
+    {
+      text: "Supported devices",
+      link: "/devices/",
+      children: deviceList,
+    },
+  ],
+  "/log/": [
+    {
+      text: "Change log",
+      link: "/log/",
+      children: getFiles("log"),
+    },
+  ],
 };
